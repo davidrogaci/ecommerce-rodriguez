885126bc77cd45ab3a26553df8545505042f175e
David <davidrogaci5@gmail.com>
2024-07-23 20:25:26 -0300

    Se agrega LOCALSTORAGE

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,7 +3,9 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
 
   const addToCart = (product) => {
     let existe = isInCart(product.id);
@@ -19,13 +21,16 @@ const CartContextProvider = ({ children }) => {
         }
       });
       setCart(newArray);
+      localStorage.setItem("cart", JSON.stringify(newArray));
     } else {
       setCart([...cart, product]);
+      localStorage.setItem("cart", JSON.stringify([...cart, product]));
     }
   };
 
   const clearCart = () => {
     setCart([]);
+    localStorage.removeItem("cart");
   };
 
   const isInCart = (id) => {
@@ -37,6 +42,7 @@ const CartContextProvider = ({ children }) => {
 
     let newArr = cart.filter((elemento) => elemento.id !== id);
     setCart(newArr);
+    localStorage.setItem("cart", JSON.stringify(newArr));
   };
 
   const getQuantityById = (id) => {
